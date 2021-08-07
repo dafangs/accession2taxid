57cdaf4315c787999bacef69dbeb7701c5384772
dafangs <dafangs@hotmail.com>
2021-08-07 12:30:31 +0800

    去除保存到redis的功能

--- README.md
+++ README.md
@@ -8,45 +8,19 @@ pip install accession2taxid
 
 ## usage
 
-### SQLite
-
-#### saving
+### saving
 
 ```
-accession2taxid sqlite -i prot.accession2taxid
+accession2taxid -i prot.accession2taxid.FULL
 ```
 
-#### querying
+### querying
 
 ```python
-from accession2taxid import get_sqlite_scoped_session, Accession2Taxid
+from accession2taxid import get_scoped_session, Accession2Taxid
 
-with get_sqlite_scoped_session() as session:
+with get_scoped_session() as session:
     session.query(Accession2Taxid).filter_by(
         accession='A0A0A0MTA4'
     )
-    session.query(Accession2Taxid).filter_by(
-        accession_version='A0A0A0MTA4.2'
-    )
-    session.query(Accession2Taxid).filter_by(
-        gi='1679377559'
-    )
-```
-
-### Redis
-
-#### saving
-
-```
-accession2taxid redis -i prot.accession2taxid
-```
-
-#### querying
-
-```python
-from accession2taxid import REDIS_KEY, get_redis_conn
-
-
-with get_redis_conn() as conn:
-    conn.hget(REDIS_KEY, 'xxx')
 ```
--- accession2taxid.py
+++ accession2taxid.py
@@ -3,14 +3,12 @@ from os import makedirs
 from contextlib import contextmanager
 from os.path import expanduser, exists, join
 
-import redis
 import click
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import Column, Integer, String, create_engine
 
 
 DBFILE = 'accession2taxid.db'
-REDIS_KEY = 'accession2taxid'
 ACCESSION2TAXID_DIR = '~/.accession2taxid'
 
 
@@ -29,40 +27,23 @@ def get_dbfile():
     return join(get_accession2taxid_dir(), DBFILE)
 
 
-def get_sqlite_engine():
+def get_engine():
     return create_engine(
         'sqlite:///%s' % get_dbfile()
     )
 
 
-def get_sqlite_session():
-    return sessionmaker(bind=get_sqlite_engine())()
+def get_session():
+    return sessionmaker(bind=get_engine())()
 
 
 @contextmanager
-def get_sqlite_scoped_session():
-    session = get_sqlite_session()
+def get_scoped_session():
+    session = get_session()
     try:
         yield session
     finally:
         session.close()
-
-
-@contextmanager
-def get_redis_conn():
-    conn = redis.StrictRedis.from_url(
-        'redis://localhost:6379',
-        decode_responses=True
-    )
-    try:
-        conn.ping()
-    except redis.ConnectionError:
-        raise Accession2TaxidError('Please start Redis server first.')
-
-    try:
-        yield conn
-    finally:
-        conn.close()
 
 
 Base = declarative_base()
@@ -73,64 +54,36 @@ class Accession2Taxid(Base):
     __tablename__ = 'accession2taxid'
     id = Column(Integer, primary_key=True)
     accession = Column(String(64), unique=True)
-    accession_version = Column(String(64), unique=True)
     taxid = Column(String(64), index=True)
-    gi = Column(String(64), unique=True)
 
     def __repr__(self):
         return f'<Accession2TaxId({self.accession})>'
 
 
-@click.group()
-def accession2taxid():
-    """Saving accession2taxid file to SQLite or Redis for querying"""
-
-
-@accession2taxid.command('sqlite')
+@click.command()
 @click.option(
     '-i', '--infile',
     type=click.Path(exists=True),
     help='the accession2taxid file downloaded from NCBI FTP site.'
 )
-def to_sqlite(infile):
+def accession2taxid(infile):
     """Saving to SQLite"""
-    Base.metadata.create_all(get_sqlite_engine())
-    with open(infile) as fp, get_sqlite_scoped_session() as session:
+    Base.metadata.create_all(get_engine())
+    with open(infile) as fp, get_scoped_session() as session:
         fp.readline()  # omit header
 
         id = 0
         for line in fp:
             id += 1
-            accession, accession_version, taxid, gi = line.rstrip().split('\t')
+            accession, taxid = line.rstrip().split('\t')
             session.add(Accession2Taxid(
                 id=id,
                 accession=accession,
-                accession_version=accession_version,
                 taxid=taxid,
-                gi=gi
             ))
             if id % 100000 == 0:
                 session.commit()
         session.commit()
-        print(f'{id} accession2taxid saved.')
-
-
-@accession2taxid.command('redis')
-@click.option(
-    '-i', '--infile',
-    type=click.Path(exists=True),
-    help='the accession2taxid file downloaded from NCBI FTP site.'
-)
-def to_redis(infile):
-    """Saving to Redis"""
-    with open(infile) as fp, get_redis_conn() as conn:
-        fp.readline()  # omit header
-
-        id = 0
-        for line in fp:
-            id += 1
-            accession, accession_version, taxid, gi = line.rstrip().split('\t')
-            conn.hset(REDIS_KEY, accession, taxid)
         print(f'{id} accession2taxid saved.')
 
 
